63ccaf01b74b88f681afbe332d7f015dc318e0e2
coolingfan21 <coolingfan21@gmail.com>
2020-01-14 16:14:31 +0900

    initial setting w/eslint,prettier + user routes

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+// const FileStore = require("session-file-store")(session);
+
+const morgan = require('morgan');
+const userRouter = require('./routes/user');
+const bucketsRouter = require('./routes/buckets');
+
+// const { urls } = require("./models");
+
+const app = express();
+const port = 3001;
+
+app.use(
+  session({
+    secret: '@halibolisky',
+    resave: false,
+    saveUninitialized: true,
+    // ,store: new FileStore()
+  }),
+);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }),
+);
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Success');
+});
+
+app.use('/user', userRouter);
+app.use('/buckets', bucketsRouter);
+
+app.set('port', port);
+app.listen(app.get('port'), () => {
+  console.log(`app is listening in PORT ${app.get('port')}`);
+});
+
+module.exports = app;
