b8c4c84ad43dacefb7596bbf1d308fd4864e23ab
coolingfan21 <coolingfan21@gmail.com>
2020-01-15 15:54:11 +0900

    Set sequelize for DB :art::heavy_plus_sign:

---
+++ migrations/20200115050554-create-users.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.NUMBER
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      signupDate: {
+        type: Sequelize.DATE
+      },
+      isBlack: {
+        type: Sequelize.STRING
+      },
+      blackReason: {
+        type: Sequelize.STRING
+      },
+      auth: {
+        type: Sequelize.STRING
+      },
+      like_bucket: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
