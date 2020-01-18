f9b8b4486e95aa71548648d6b5107b063a8e75e7
hyunsung <hslgood@gmail.com>
2020-01-18 22:03:39 +0900

    fix get pid data logic and add token handle

--- controller/buckets/home.js
+++ controller/buckets/home.js
@@ -3,15 +3,27 @@
 /* eslint-disable arrow-parens */
 /* eslint-disable object-curly-newline */
 const { bucketlists, Sequelize, users, likes } = require('../../models');
+const { isValid } = require('../../utils/tokenhelper');
 
 module.exports = {
   get: (req, res) => {
+    // 로그인 안되어있는 상태
+    const { token } = req.cookies;
+
     const result = { bucketlist: [] };
+
+    let userId = null;
+
+    if (token) {
+      isValid(token, validToken => {
+        userId = validToken.userInfo.id;
+      });
+    }
 
     bucketlists
       .findAll({
-        order: Sequelize.literal('rand()'),
-        limit: 1,
+        order: Sequelize.random(),
+        limit: 8,
         include: [users],
       })
       .then(data => {
@@ -24,22 +36,27 @@ module.exports = {
           obj.userNickName = ele.userNickName;
           obj.expectedDate = ele.expectedDate;
           obj.createdAt = ele.createdAt;
+          obj.likeCount = ele.likeCount;
+          obj.mylike = false;
 
-          likes
-            .findAndCountAll({
-              include: [bucketlists],
-              where: {
-                bucket_id: ele.id,
-              },
-            })
-            .then(likeData => {
-              obj.likes = likeData.count;
+          if (userId) {
+            likes.findOne({ where: { bucket_id: ele.id } }).then(likedata => {
+              if (likedata.user_id === userId) {
+                obj.mylike = true;
+              }
               result.bucketlist.push(obj);
             });
+          } else {
+            result.bucketlist.push(obj);
+          }
         });
       })
       .then(() => {
         res.send(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(404).send(err);
       });
   },
 };
