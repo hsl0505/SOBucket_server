daddfe43da9fbad1624b85c42276c26da891d188
hyunsung <hslgood@gmail.com>
2020-01-22 19:55:27 +0900

    fix mypage

--- controller/buckets/mypage.js
+++ controller/buckets/mypage.js
@@ -1,23 +1,47 @@
-const { bucketlists } = require('../../models');
+const { bucketlists, users, likes } = require('../../models');
 
 const { isValid } = require('../../utils/tokenhelper');
 
 module.exports = {
   get: (req, res) => {
     const { token } = req.cookies;
+
     let userId = 'no_user_id';
     if (token) {
       isValid(token, validToken => {
         userId = validToken.userInfo.id;
       });
     }
+
+    const resObj = { bucketlists: [] };
+
     bucketlists
       .findAll({
+        include: [
+          { model: users, attributes: ['userNickName', 'avatar'] },
+          { model: likes, attributes: ['user_id'] },
+        ],
         where: { user_id: userId },
       })
-      .then(result => {
-        res.status(200).json({ bucketlists: result });
+      .then(data => {
+        for (let i = 0; i < data.length; i++) {
+          const obj = data[i].dataValues;
+          obj.mylike = false;
+          for (let j = 0; j < data[i].likes.length; j++) {
+            if (data[i].likes[j].user_id === userId) {
+              obj.mylike = true;
+              break;
+            }
+          }
+          resObj.bucketlists.push(obj);
+        }
       })
-      .catch(err => res.sendStatus(400));
+      .then(() => {
+        res.status(200).json(resObj);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(404);
+      });
   },
 };
